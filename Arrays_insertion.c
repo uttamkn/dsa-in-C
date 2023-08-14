3e0272e2d56ccedb9ce53323ca919cd247ca3e7c
Draco156 <uttamkn15@gmail.com>
2023-08-14 23:57:50 +0530

    Added array insertion file

---
+++ Arrays_insertion.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+void disp(int arr[], int n)
+{
+    for(int i=0;i<n;i++)
+    {
+    printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+int indInsert(int arr[],int size,int element,int index,int capacity)
+{
+    if(size>=capacity){
+        return -1;
+    }
+    else {
+        for(int i=size-1;i>=index;i--){
+            arr[i+1]=arr[i];
+        }
+        arr[index]=element;
+
+        return 1;
+    }
+}
+
+int sortIndex(int arr[],int size,int element){
+    for(int i=0;i<size;i++){
+        if(arr[i]>element){
+            return i;
+        }
+    }
+}
+
+int main()
+{
+    int arr[100]={2,4,5,62,78};
+    int size=5,index,element;
+
+    disp(arr,size);
+    
+    printf("Enter the element to be insterted: ");
+    scanf("%d",&element);
+    printf("Enter the index at which the element should be inserted: ");    //comment this for sorted insertion
+    scanf("%d",&index);                                                                     
+    
+    //sorted insertion (array should be sorted)
+    // index=sortIndex(arr,size,element);                                   //uncomment for sorted insertion
+
+
+    int res=indInsert(arr,size,element,index,100);
+    if(res==1){
+        size+=1;
+        disp(arr,size);
+    }
+    else {
+        printf("Insertion Failed!\n");
+    }
+
+    return 0;
+}
