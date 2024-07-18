47ef224a28aa2491a9623df853f63c13ea6799ab
uttamkn <uttamkn15@gmail.com>
2024-07-18 22:55:37 +0530

    add knapsack

--- dynamic_programming/knapsack.c
+++ dynamic_programming/knapsack.c
@@ -5,20 +5,31 @@ int max(int a, int b) {
     return a > b ? a : b;
 }
 
+// int knapsack(int *weights, int *values, int n, int W) {
+//     int dp[n + 1][W + 1];
+//     for (int i = 0; i <= n; i++) {
+//         for (int w = 0; w <= W; w++) {
+//             if (i == 0 || w == 0) {
+//                 dp[i][w] = 0;
+//             } else if (weights[i - 1] <= w) {
+//                 dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+//             } else {
+//                 dp[i][w] = dp[i - 1][w];
+//             }
+//         }
+//     }
+//     return dp[n][W];
+// }
+
 int knapsack(int *weights, int *values, int n, int W) {
-    int dp[n + 1][W + 1];
-    for (int i = 0; i <= n; i++) {
-        for (int w = 0; w <= W; w++) {
-            if (i == 0 || w == 0) {
-                dp[i][w] = 0;
-            } else if (weights[i - 1] <= w) {
-                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
-            } else {
-                dp[i][w] = dp[i - 1][w];
-            }
-        }
-    }
-    return dp[n][W];
+
+if(n == 0 || W == 0) return 0;
+
+ if(weights[n-1] > W) {
+    return knapsack(weights, values, n-1, W);
+ } else {
+    return max(values[n-1] + knapsack(weights, values, n-1, W-weights[n-1]), knapsack(weights, values, n-1, W));
+ }
 }
 
 int main() {
