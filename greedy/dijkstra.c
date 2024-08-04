cef95ab674c4af00c3aa1b813b061413dde4b16d
uttamkn <uttamkn15@gmail.com>
2024-08-04 20:12:54 +0530

    update dijkstra

--- greedy/dijkstra.c
+++ greedy/dijkstra.c
@@ -4,86 +4,83 @@
 #define INFINITY 9999
 #define MAX 10
 
-void Dijkstra(int **Graph, int n, int start) {
-  int cost[MAX][MAX], distance[MAX], pred[MAX];
-  int visited[MAX], count, mindistance, nextnode, i, j;
+void dijkstra(int **graph, int n, int start) {
+  int cost[MAX][MAX], distance[MAX], visited[MAX];
 
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
-      if (Graph[i][j] == 0) {
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (graph[i][j] == 0) {
         cost[i][j] = INFINITY;
       } else {
-        cost[i][j] = Graph[i][j];
+        cost[i][j] = graph[i][j];
       }
     }
   }
 
-  for (i = 0; i < n; i++) {
+  for (int i = 0; i < n; ++i) {
     distance[i] = cost[start][i];
-    pred[i] = start;
     visited[i] = 0;
   }
 
   distance[start] = 0;
   visited[start] = 1;
-  count = 1;
+  int count = 1;
 
   while (count < n - 1) {
-    mindistance = INFINITY;
+    int mindistance = INFINITY, nextNode;
 
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       if (distance[i] < mindistance && !visited[i]) {
         mindistance = distance[i];
-        nextnode = i;
+        nextNode = i;
       }
     }
 
-    visited[nextnode] = 1;
+    visited[nextNode] = 1;
 
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       if (!visited[i]) {
-        if (mindistance + cost[nextnode][i] < distance[i]) {
-          distance[i] = mindistance + cost[nextnode][i];
-          pred[i] = nextnode;
+        if (mindistance + cost[nextNode][i] < distance[i]) {
+          distance[i] = mindistance + cost[nextNode][i];
         }
       }
     }
     count++;
   }
 
-  for (i = 0; i < n; i++) {
+  for (int i = 0; i < n; ++i) {
     if (i != start) {
-      printf("\nDistance from source to %d: %d", i, distance[i]);
+      printf("distance from source to %d: %d\n", i, distance[i]);
     }
   }
 }
 
 int main() {
-  int **Graph, i, j, n, u;
+  int **graph, i, j, n, u;
 
   printf("Enter the number of vertices: ");
   scanf("%d", &n);
 
-  Graph = (int **)malloc(n * sizeof(int *));
+  graph = (int **)malloc(n * sizeof(int *));
   for (i = 0; i < n; i++) {
-    Graph[i] = (int *)malloc(n * sizeof(int));
+    graph[i] = (int *)malloc(n * sizeof(int));
   }
 
   printf("Enter the adjacency matrix:\n");
   for (i = 0; i < n; i++) {
     for (j = 0; j < n; j++) {
-      scanf("%d", &Graph[i][j]);
+      scanf("%d", &graph[i][j]);
     }
   }
 
   u = 0;
 
-  Dijkstra(Graph, n, u);
+  dijkstra(graph, n, u);
 
   for (i = 0; i < n; i++) {
-    free(Graph[i]);
+    free(graph[i]);
   }
-  free(Graph);
+  free(graph);
 
   return 0;
 }
