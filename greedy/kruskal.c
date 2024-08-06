42245e8fd4a3cf69a476ea97ef37fca38ca8f31d
uttamkn <uttamkn15@gmail.com>
2024-08-06 12:48:23 +0530

    formatting

--- greedy/kruskal.c
+++ greedy/kruskal.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 #define MAX 30
 
-typedef struct
-{
-    int u, v, w;
+typedef struct {
+  int u, v, w;
 } edge;
 
-typedef struct
-{
-    edge data[MAX];
-    int n;
+typedef struct {
+  edge data[MAX];
+  int n;
 } edge_list;
 
 edge_list elist;
@@ -22,117 +20,95 @@ void applyUnion(int belongs[], int c1, int c2, int n);
 void sort();
 void print();
 
-void kruskal(int **graph, int numVerts)
-{
-    int belongs[numVerts];
-    elist.n = 0;
+void kruskal(int **graph, int numVerts) {
+  int belongs[numVerts];
+  elist.n = 0;
 
-    for (int i = 1; i < numVerts; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (graph[i][j] != 0)
-            {
-                elist.data[elist.n].u = i;
-                elist.data[elist.n].v = j;
-                elist.data[elist.n].w = graph[i][j];
-                elist.n++;
-            }
-        }
+  for (int i = 1; i < numVerts; i++) {
+    for (int j = 0; j < i; j++) {
+      if (graph[i][j] != 0) {
+        elist.data[elist.n].u = i;
+        elist.data[elist.n].v = j;
+        elist.data[elist.n].w = graph[i][j];
+        elist.n++;
+      }
     }
+  }
 
-    sort();
+  sort();
 
-    for (int i = 0; i < numVerts; i++)
-    {
-        belongs[i] = i;
+  for (int i = 0; i < numVerts; i++) {
+    belongs[i] = i;
+  }
+
+  spanlist.n = 0;
+  int cno1, cno2;
+
+  for (int i = 0; i < elist.n; i++) {
+    cno1 = find(belongs, elist.data[i].u);
+    cno2 = find(belongs, elist.data[i].v);
+
+    if (cno1 != cno2) {
+      spanlist.data[spanlist.n] = elist.data[i];
+      spanlist.n++;
+      applyUnion(belongs, cno1, cno2, numVerts);
     }
-
-    spanlist.n = 0;
-    int cno1, cno2;
-
-    for (int i = 0; i < elist.n; i++)
-    {
-        cno1 = find(belongs, elist.data[i].u);
-        cno2 = find(belongs, elist.data[i].v);
-
-        if (cno1 != cno2)
-        {
-            spanlist.data[spanlist.n] = elist.data[i];
-            spanlist.n++;
-            applyUnion(belongs, cno1, cno2, numVerts);
-        }
-    }
+  }
 }
 
-int find(int belongs[], int vertex)
-{
-    return (belongs[vertex]);
+int find(int belongs[], int vertex) { return (belongs[vertex]); }
+
+void applyUnion(int belongs[], int c1, int c2, int n) {
+  for (int i = 0; i < n; i++) {
+    if (belongs[i] == c2) {
+      belongs[i] = c1;
+    }
+  }
 }
 
-void applyUnion(int belongs[], int c1, int c2, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        if (belongs[i] == c2)
-        {
-            belongs[i] = c1;
-        }
+void sort() {
+  edge temp;
+  for (int i = 0; i < elist.n; i++) {
+    for (int j = 0; j < elist.n - 1 - i; j++) {
+      if (elist.data[j].w > elist.data[j + 1].w) {
+        temp = elist.data[j];
+        elist.data[j] = elist.data[j + 1];
+        elist.data[j + 1] = temp;
+      }
     }
+  }
 }
 
-void sort()
-{
-    edge temp;
-    for (int i = 1; i < elist.n; i++)
-    {
-        for (int j = 0; j < elist.n - 1; j++)
-        {
-            if (elist.data[j].w > elist.data[j + 1].w)
-            {
-                temp = elist.data[j];
-                elist.data[j] = elist.data[j + 1];
-                elist.data[j + 1] = temp;
-            }
-        }
-    }
+void print() {
+  int cost = 0;
+  for (int i = 0; i < spanlist.n; i++) {
+    printf("\n%d-%d:%d", spanlist.data[i].u, spanlist.data[i].v,
+           spanlist.data[i].w);
+    cost += spanlist.data[i].w;
+  }
+
+  printf("\nCost: %d", cost);
 }
 
-void print()
-{
-    int cost = 0;
-    for (int i = 0; i < spanlist.n; i++)
-    {
-        printf("\n%d-%d:%d", spanlist.data[i].u, spanlist.data[i].v, spanlist.data[i].w);
-        cost += spanlist.data[i].w;
+int main() {
+  int numVerts;
+  int **graph;
+  printf("numverts: ");
+  scanf("%d", &numVerts);
+
+  graph = (int **)malloc(numVerts * sizeof(int *));
+
+  for (int i = 0; i < numVerts; i++) {
+    graph[i] = (int *)malloc(numVerts * sizeof(int));
+  }
+
+  printf("adj mat");
+  for (int i = 0; i < numVerts; i++) {
+    for (int j = 0; j < numVerts; j++) {
+      scanf("%d", &graph[i][j]);
     }
+  }
 
-    printf("\nCost: %d", cost);
-}
-
-int main()
-{
-    int numVerts;
-    int **graph;
-    printf("numverts: ");
-    scanf("%d", &numVerts);
-
-    graph = (int **)malloc(numVerts * sizeof(int *));
-
-    for (int i = 0; i < numVerts; i++)
-    {
-        graph[i] = (int *)malloc(numVerts * sizeof(int));
-    }
-
-    printf("adj mat");
-    for (int i = 0; i < numVerts; i++)
-    {
-        for (int j = 0; j < numVerts; j++)
-        {
-            scanf("%d", &graph[i][j]);
-        }
-    }
-
-    kruskal(graph, numVerts);
-    print();
+  kruskal(graph, numVerts);
+  print();
 }
--- string_matching/horspool.c
+++ string_matching/horspool.c
@@ -1,53 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #define SIZE 256
 
 char table[SIZE];
 
-void shiftTable (char *pattern) {
-    int n = strlen(pattern);
-    for (int i = 0; i < SIZE; i++) {
-        table[i] = n;                             
-    }
-    
-    for (int i = 0; i < n-1; i++) {
-        table[(unsigned char)pattern[i]] = n - i - 1;
-    }
+void shiftTable(char *pattern) {
+  int n = strlen(pattern);
+  for (int i = 0; i < SIZE; i++) {
+    table[i] = n;
+  }
+
+  for (int i = 0; i < n - 1; i++) {
+    table[(unsigned char)pattern[i]] = n - i - 1;
+  }
 }
 
-int horspools (char *source, char *pattern) {
-    shiftTable(pattern);
-    int n = strlen(source);
-    int m = strlen(pattern);
-    int i = m - 1;
+int horspools(char *source, char *pattern) {
+  shiftTable(pattern);
+  int n = strlen(source);
+  int m = strlen(pattern);
+  int i = m - 1;
 
-    while (i < n) {
-        int k = 0;
-        while (k < m && pattern[m-1-k] == source[i-k]) {
-            k++;
-        }
-
-        if (k == m) {
-            return i - m + 1;
-        } else {
-            i += table[(unsigned char)source[i]];
-        }
+  while (i < n) {
+    int k = 0;
+    while (k < m && pattern[m - 1 - k] == source[i - k]) {
+      k++;
     }
 
-    return -1;
-}
-
-int main () {
-    char src[] = "ATCGGCTAAGCTTA";
-    char pattern[] = "AAGC";
-
-    int idx = horspools(src, pattern);
-
-    if (idx == -1) {
-        printf("not found\n");
+    if (k == m) {
+      return i - m + 1;
     } else {
-        printf("found at %d\n", idx);
+      i += table[(unsigned char)source[i]];
     }
-    return 0;
+  }
+
+  return -1;
+}
+
+int main() {
+  char src[] = "ATCGGCTAAGCTTA";
+  char pattern[] = "AAGC";
+
+  int idx = horspools(src, pattern);
+
+  if (idx == -1) {
+    printf("not found\n");
+  } else {
+    printf("found at %d\n", idx);
+  }
+  return 0;
 }
